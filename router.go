b225714ee819f30bdc71abbf80ee663c20f0097f
Rodrigo Moraes <rodrigo.moraes@gmail.com>
2015-10-28 08:26:21 -0200

    Using anonymous functions for handlers; and other small things.

--- router.go
+++ router.go
@@ -1,8 +1,10 @@
 package muxy
 
 import (
+	"fmt"
 	"net/http"
 	"net/url"
+	"strings"
 )
 
 // New creates a new Router.
@@ -60,7 +62,7 @@ func (s *Subrouter) Route(pattern string) *Route {
 
 // Sub creates a subrouter for the given pattern prefix.
 func (s *Subrouter) Sub(pattern string) *Subrouter {
-	return &Subrouter{router: s.router, prefix: s.prefix + pattern}
+	return s.router.Sub(s.prefix + pattern)
 }
 
 // -----------------------------------------------------------------------------
@@ -68,14 +70,14 @@ func (s *Subrouter) Sub(pattern string) *Subrouter {
 // newRoute creates a new Route.
 func newRoute(r *Router, pattern string) *Route {
 	// not implemented...
-	return &Route{router: r, methods: map[string]http.HandlerFunc{}}
+	return &Route{router: r, methods: map[string]func(http.ResponseWriter, *http.Request){}}
 }
 
 // Route stores a URL pattern to be matched and the handler to be served
 // in case of a match, optionally mapping HTTP methods to different handlers.
 type Route struct {
 	router  *Router
-	methods map[string]http.HandlerFunc
+	methods map[string]func(http.ResponseWriter, *http.Request)
 	// ...
 }
 
@@ -86,13 +88,13 @@ func (r *Route) Name(name string) *Route {
 }
 
 // Handler sets the given handler to be served for the optional request methods.
-func (r *Route) Handler(handler http.HandlerFunc, methods ...string) *Route {
-	// not implemented...
-	if len(methods) == 0 {
-		methods = []string{""}
-	}
-	for _, m := range methods {
-		r.methods[m] = handler
+func (r *Route) Handler(handler func(http.ResponseWriter, *http.Request), methods ...string) *Route {
+	if methods == nil {
+		r.methods[""] = handler
+	} else {
+		for _, m := range methods {
+			r.methods[m] = handler
+		}
 	}
 	return r
 }
@@ -101,46 +103,77 @@ func (r *Route) Handler(handler http.HandlerFunc, methods ...string) *Route {
 // to call r.Handler(handler, "METHOD-NAME").
 
 // Connect sets the given handler to be served for the request method CONNECT.
-func (r *Route) Connect(handler http.HandlerFunc) *Route {
+func (r *Route) Connect(handler func(http.ResponseWriter, *http.Request)) *Route {
 	return r.Handler(handler, "CONNECT")
 }
 
 // Delete sets the given handler to be served for the request method DELETE.
-func (r *Route) Delete(handler http.HandlerFunc) *Route {
+func (r *Route) Delete(handler func(http.ResponseWriter, *http.Request)) *Route {
 	return r.Handler(handler, "DELETE")
 }
 
 // Get sets the given handler to be served for the request method GET.
-func (r *Route) Get(handler http.HandlerFunc) *Route {
+func (r *Route) Get(handler func(http.ResponseWriter, *http.Request)) *Route {
 	return r.Handler(handler, "GET")
 }
 
 // Head sets the given handler to be served for the request method HEAD.
-func (r *Route) Head(handler http.HandlerFunc) *Route {
+func (r *Route) Head(handler func(http.ResponseWriter, *http.Request)) *Route {
 	return r.Handler(handler, "HEAD")
 }
 
 // Options sets the given handler to be served for the request method OPTIONS.
-func (r *Route) Options(handler http.HandlerFunc) *Route {
+func (r *Route) Options(handler func(http.ResponseWriter, *http.Request)) *Route {
 	return r.Handler(handler, "OPTIONS")
 }
 
 // PATCH sets the given handler to be served for the request method PATCH.
-func (r *Route) Patch(handler http.HandlerFunc) *Route {
+func (r *Route) Patch(handler func(http.ResponseWriter, *http.Request)) *Route {
 	return r.Handler(handler, "PATCH")
 }
 
 // POST sets the given handler to be served for the request method POST.
-func (r *Route) Post(handler http.HandlerFunc) *Route {
+func (r *Route) Post(handler func(http.ResponseWriter, *http.Request)) *Route {
 	return r.Handler(handler, "POST")
 }
 
 // Put sets the given handler to be served for the request method PUT.
-func (r *Route) Put(handler http.HandlerFunc) *Route {
+func (r *Route) Put(handler func(http.ResponseWriter, *http.Request)) *Route {
 	return r.Handler(handler, "PUT")
 }
 
 // Trace sets the given handler to be served for the request method TRACE.
-func (r *Route) Trace(handler http.HandlerFunc) *Route {
+func (r *Route) Trace(handler func(http.ResponseWriter, *http.Request)) *Route {
 	return r.Handler(handler, "TRACE")
+}
+
+// methodHandler returns the handler registered for the given HTTP method.
+func (r *Route) methodHandler(method string) func(http.ResponseWriter, *http.Request) {
+	if h, ok := r.methods[method]; ok {
+		return h
+	}
+	if h, ok := r.methods[""]; ok {
+		return h
+	}
+	if method == "OPTIONS" {
+		return r.allowHandler(200)
+	}
+	return r.allowHandler(405)
+}
+
+// allowHandler returns a handler that sets a header with the given
+// status code and allowed methods.
+func (r *Route) allowHandler(code int) func(http.ResponseWriter, *http.Request) {
+	allowed := []string{"OPTIONS"}
+	for m, _ := range r.methods {
+		if m != "" && m != "OPTIONS" {
+			allowed = append(allowed, m)
+		}
+	}
+	return func(w http.ResponseWriter, r *http.Request) {
+		w.Header().Set("Content-Type", "text/plain; charset=utf-8")
+		w.Header().Set("Allow", strings.Join(allowed, ", "))
+		w.WriteHeader(code)
+		fmt.Fprintln(w, code, http.StatusText(code))
+	}
 }
